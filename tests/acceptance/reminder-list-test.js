34cdc254686118a305ed33ccdc3484515dd84c9f
Casey Cross <hellocaseycross@gmail.com>
2016-10-12 14:43:54 -0600

    Initial commit

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -3,7 +3,7 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'remember/tests/helpers/module-for-acceptance';
 
-import Ember from 'ember';
+import Ember from 'ember';  
 
 moduleForAcceptance('Acceptance | reminders list');
 
