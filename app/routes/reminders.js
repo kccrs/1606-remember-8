25976bb98b42d55b34d8fff3672e7f5640f3644d
Casey Cross <hellocaseycross@gmail.com>
2016-10-12 20:25:25 -0600

    Fix code review issues.

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,7 +2,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function () {
-    return this.store.findAll('reminder');
+  model () {
+    return this.get('store').findAll('reminder');
   }
 });
