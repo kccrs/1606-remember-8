ceea5d43940e0ba328091397a0d76263444808fd
Casey Cross <hellocaseycross@gmail.com>
2016-10-13 16:16:52 -0600

    Fix date not displaying

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -4,6 +4,7 @@ export default Ember.Controller.extend({
   actions: {
     addReminder() {
       const reminder = this.getProperties('title', 'date', 'notes');
+      reminder.date = new Date(reminder.date);
       this.get('store').createRecord('reminder', reminder).save().then(() => {
         this.setProperties({ title: '', date: '', notes: '' });
       });
