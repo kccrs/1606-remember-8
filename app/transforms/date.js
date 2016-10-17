08aec21b8ac19c53be62d19fb6dba6be60273622
Casey Cross <hellocaseycross@gmail.com>
2016-10-17 17:26:13 -0600

    Gave the transform file some breathing room.

--- app/transforms/date.js
+++ app/transforms/date.js
@@ -2,13 +2,20 @@ import DS from 'ember-data';
 import moment from 'moment';
 
 export default DS.Transform.extend({
+
  deserialize(serialized) {
-   if (serialized){return moment(serialized).format("dddd, MMM Do, YYYY");}
+
+   if (serialized) {
+     return moment(serialized).format("dddd, MMM Do, YYYY");
+   }
    return serialized;
  },
 
  serialize(deserialized){
-   if(deserialized){return moment(deserialized).toISOString();}
+
+   if(deserialized) {
+     return moment(deserialized).toISOString();
+   }
    return deserialized;
  }
 });
