1f1df55941f75862bb3087a2d690a4758cceb830
Casey Cross <hellocaseycross@gmail.com>
2016-10-16 20:28:02 -0600

    Layout begins to behave properly

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -2,8 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  // format: "YYYYMMDD",
+  //  date: null,
+  //  formattedDate: function() {
+  //    var date = this.get('date'),
+  //        format = this.get('format');
+  //    return moment(date).format(format);
+  //  }.property('date', 'format')
+
   actions: {
     addReminder() {
+
       const reminder = this.getProperties('title', 'date', 'notes');
       reminder.date = new Date(reminder.date);
       this.get('store').createRecord('reminder', reminder).save().then(() => {
@@ -12,3 +21,14 @@ export default Ember.Controller.extend({
     }
   }
 });
+
+
+// App.ApplicationController = Ember.Controller.extend({
+//   format: "YYYYMMDD",
+//   date: null,
+//   formattedDate: function() {
+//     var date = this.get('date'),
+//         format = this.get('format');
+//     return moment(date).format(format);
+//   }.property('date', 'format')
+// });
